106c9446236929c0079d91125c523e324612e9cd
Agustin <agustin@atmosfera.unam.mx>
2024-01-29 14:36:22 -0600

    From UTF-8 a roman

--- convierte-UTF8.sh
+++ convierte-UTF8.sh
@@ -3,5 +3,5 @@
 # Codigo para convertir archivos codificados en Roman a UTF-8
 # Busca todos lo archivos en tex y los transforma a de macintosh a UTF-8
 #
-find . -name "*.tex" -exec bash -c 'mv $1 $1.WXY; iconv -f macintosh  -t UTF-8 $1.WXY > $1; rm $1.WXY' sh {} \;
+find . -name "*.tex" -exec bash -c 'mv $1 $1.WXY; iconv -f UTF-8  -t macintosh $1.WXY > $1; rm $1.WXY' sh {} \;
 
